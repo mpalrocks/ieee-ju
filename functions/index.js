c4e8c99da7f009ee1bfc831c108995a6a1c1df83
digantaTheProgrammer <digantamandal28@gmail.com>
2019-03-20 20:56:00 +0530

    BACKEND COMMIT

---
+++ functions/index.js
@@ -0,0 +1,44 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const app=express();
+app.get('/',(req,res)=>{
+const date = new Date();
+const hours = date.getHours();
+const mins=date.getMinutes();
+const secs=date.getSeconds();
+  res.send(`
+    <!doctype html>
+    <head>
+      <title>Time</title>
+      <link rel="stylesheet" href="/style.css">
+      <script src="/script.js"></script>
+    </head>
+    <body>
+      <p>In London, the clock strikes:
+        <span id="bongs">${hours} : ${mins} : ${secs}</span></p>
+      <button onClick="refresh(this)">Refresh</button>
+    </body>
+  </html>`);
+});
+
+app.get('/:name',(req,res)=>{
+const date = new Date();
+const hours = date.getHours();
+const mins=date.getMinutes();
+const secs=date.getSeconds();
+  res.send(`
+    <!doctype html>
+    <head>
+      <title>Time</title>
+      <link rel="stylesheet" href="/style.css">
+      <script src="/script.js"></script>
+    </head>
+    <body>
+      <p>In ${req.params.name}, the clock strikes:
+        <span id="bongs">${hours} : ${mins} : ${secs}</span></p>
+      <button onClick="refresh(this)">Refresh</button>
+    </body>
+  </html>`);
+});
+
+exports.fun=functions.https.onRequest(app);
